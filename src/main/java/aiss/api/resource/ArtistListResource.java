b4989f9ed3ae55eae5aa828ea092703549575d6b
AntonioJesusGarciaNieto <45717153+AntonioJesusGarciaNieto@users.noreply.github.com>
2019-05-14 20:49:28 +0200

    1

--- src/main/java/aiss/api/resource/ArtistListResource.java
+++ src/main/java/aiss/api/resource/ArtistListResource.java
@@ -1,11 +1,14 @@
 package aiss.api.resource;
 
 import java.net.URI;
+import java.util.Collection;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriBuilder;
@@ -13,6 +16,8 @@ import javax.ws.rs.core.UriInfo;
 
 import org.jboss.resteasy.spi.BadRequestException;
 
+import aiss.api.model.Artist;
+import aiss.api.model.ArtistList;
 import javassist.NotFoundException;
 
 public class ArtistListResource {
@@ -41,22 +46,22 @@ public class ArtistListResource {
 	@Produces("application/json")
 	public Artist get(@PathParam("id") String id){
 		Artist artista = repository.getArtist(id);
-		if(list== null) {
+		if( artista== null) {
 			throw new NotFoundException("El artista con el id -> "+ id + " no existe. Has probado con Melendi :D");
 		}
-		return artist;
+		return artista;
 		}
 	@GET
 	@Consumes("application/json")
 	@Produces("application/json")
 	public Response addArtist(@Context UriInfo uriInfo,Artist artist){
-		if(artist.getName()= null || "".equals(artist.getName())) {
+		if(Artist.getNombre()== null || "".equals(Artist.getNombre())) {
 			throw new BadRequestException("El nombre del artista no puede ser null ni una cadena vacía.");
 		}
 		repository.addArtist(artist);
 		
 		UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(),"get");
-		URI uri = ub.build(artist.getId());
+		URI uri = ub.build(Artist.getId());
 		ResponseBuilder resp = Response.created(uri);
 		return resp.build();
 		}
